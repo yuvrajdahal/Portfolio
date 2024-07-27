5a0a6ac8ae0e6ec72ce2418760d5332891cc85b7
yuvrajdahal <iamyuvraj456@gmail.com>
2024-07-27 21:16:13 +0545

    fix:remove test code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import {
 import { ProjectSection } from "./components/project-section";
 
 export default function Home() {
-  const [sectionIndex, setSectionIndex] = useState<number>(1);
+  const [sectionIndex, setSectionIndex] = useState<number>(0);
   const [prevSectionIndex, setPrevSectionIndex] = useState<number>(0);
 
   function handleSectionChange(index: number) {
