1cc297e377bd17d643fce1b418461521b626b7ff
yuvrajdahal <iamyuvraj456@gmail.com>
2024-08-02 19:55:36 +0545

    style:animation

--- src/app/components/about-section.tsx
+++ src/app/components/about-section.tsx
@@ -12,7 +12,12 @@ export const AboutSection = ({}) => {
     <>
       <section className="h-full duration-300 transition  relative flex flex-col items-center ">
         <div className="absolute -left-[4rem] flex items-center relative justify-center">
-          <div className="overflow-hidden relative left-[10rem] h-[200px] w-[300px] z-[50] bg-gray-200 rounded-md">
+          <motion.div
+            initial={{ opacity: 0, y: "-2vh" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            className="overflow-hidden relative left-[10rem] h-[200px] w-[300px] z-[50] bg-gray-200 rounded-md"
+          >
             <Image
               src={"/yuvraj-2.jpg"}
               className="object-left transition	hover:scale-110  h-full w-full object-cover"
@@ -20,8 +25,13 @@ export const AboutSection = ({}) => {
               width={200}
               alt="yuvraj"
             />
-          </div>
-          <div className="overflow-hidden relative left-[5rem] h-[350px] w-[300px] bg-gray-600 rounded-md z-[100]">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: "-2vh" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            className="overflow-hidden relative left-[5rem] h-[350px] w-[300px] bg-gray-600 rounded-md z-[100]"
+          >
             <Image
               src={"/scene-2.jpg"}
               className="transition	hover:scale-110"
@@ -29,8 +39,13 @@ export const AboutSection = ({}) => {
               width={250}
               alt="yuvraj"
             />
-          </div>
-          <div className="overflow-hidden h-[400px] w-[300px] bg-gray-200 rounded-md z-[200]">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: "-2vh" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            className="overflow-hidden h-[400px] w-[300px] bg-gray-200 rounded-md z-[200]"
+          >
             <Image
               src={"/yuvraj-1.jpg"}
               className="object-left	transition	hover:scale-110 h-full w-full object-cover"
@@ -38,8 +53,13 @@ export const AboutSection = ({}) => {
               width={300}
               alt="yuvraj"
             />
-          </div>
-          <div className="overflow-hidden relative -left-[4rem] h-[200px] w-[250px] bg-gray-600 rounded-md z-[100]">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: "-2vh" }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            className="overflow-hidden relative -left-[4rem] h-[200px] w-[250px] bg-gray-600 rounded-md z-[100]"
+          >
             <Image
               src={"/scene.jpg"}
               className="object-center transition	hover:scale-110 h-full w-full object-cover"
@@ -47,49 +67,59 @@ export const AboutSection = ({}) => {
               width={250}
               alt="yuvraj"
             />
-          </div>
+          </motion.div>
         </div>
         <div className="w-[70%] pb-6">
           <div className="flex flex-col justify-center mt-12 ">
-            <h2
+            <motion.h2
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.4, delay: 0.1 }}
               className={twMerge(caesar.className, "text-4xl light-gradient")}
             >
               Hello There, I'm Yuvraj.
-            </h2>
-            <p
+            </motion.h2>
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.4, delay: 0.2 }}
               className={twMerge(
                 lato.className,
                 "text-justify text-balance mt-4 text-lg 4xl:text-2xl"
               )}
             >
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
-              numquam quisquam laboriosam enim voluptatum, est molestiae
-              impedit? Quod, explicabo autem at numquam totam nihil illum
-              voluptatibus culpa fugit accusamus, delectus accusantium enim
-              dolor corporis blanditiis. Exercitationem adipisci ratione ad
-              suscipit quia enim labore quod, illo consequuntur. Incidunt
-              laudantium a ratione rerum eum unde maxime? Aperiam in labore
-              totam dolore earum soluta natus velit illum mollitia veniam nobis
-              distinctio, non odio, perferendis est molestias nemo qui et neque
-              eum commodi accusamus provident? Repudiandae modi vitae a atque
-              perspiciatis impedit numquam, unde sit! Quasi, necessitatibus rem?
-              Nisi harum nostrum ratione. Dolores minima eaque in. Molestiae
-              reprehenderit esse officia ipsum iste. Praesentium consectetur
-              nisi, est molestiae autem cumque at eaque assumenda inventore
-              officia.
+              Yuvraj Dahal, a prominent figure in the realm of technology and
+              innovation, has made significant strides in various fields,
+              particularly in software development, artificial intelligence, and
+              digital transformation. Born and raised in Nepal, Yuvraj exhibited
+              a keen interest in technology from an early age, which eventually
+              led him to pursue a career in this dynamic field. His journey is a
+              testament to his dedication, passion, and relentless pursuit of
+              excellence. Yuvraj's academic background is as impressive as his
+              professional achievements. He holds a degree in Computer Science,
+              which provided him with a solid foundation in the principles and
+              practices of software engineering. His thirst for knowledge and
+              his drive to stay at the forefront of technological advancements
+              prompted him to continuously update his skills and expertise
+              through various certifications and advanced courses. This
+              commitment to lifelong learning has been a key factor in his
+              success, enabling him to tackle complex challenges and come up
+              with innovative solutions.
               <br />
               <br />
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
-              numquam quisquam laboriosam enim voluptatum, est molestiae
-              impedit? Quod, explicabo autem at numquam totam nihil illum
-              voluptatibus culpa fugit accusamus, delectus accusantium enim
-              dolor corporis blanditiis. Exercitationem adipisci ratione ad
-              suscipit quia enim labore quod, illo consequuntur. Incidunt
-              laudantium a ratione rerum eum unde maxime? Aperiam in labore
-              totam dolore earum soluta natus velit illum mollitia veniam nobis
-              distinctio, non odio, perferendis est molestias nemo qui et neque
-              eum commodi accusamus provident?
-            </p>
+              Yuvraj Dahal's contributions extend beyond his role as a React
+              developer. He is actively involved in community initiatives
+              promoting digital literacy and technology education. Through
+              workshops, seminars, and online courses, he has helped bridge the
+              digital divide, especially in underprivileged areas. Yuvraj
+              frequently shares his insights through blogs, articles, and
+              speaking engagements, earning respect in the tech community. His
+              innovative mindset and ability to foresee technological trends
+              have garnered recognition and accolades. Yuvraj's journey from a
+              curious young boy in Nepal to a tech expert is a source of
+              inspiration, showcasing the power of passion, perseverance, and
+              continuous learning.
+            </motion.p>
           </div>
         </div>
       </section>
